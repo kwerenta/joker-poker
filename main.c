8ad61d8856f60e30cd525f2ab7c66df5c55eae1f
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2024-12-23 16:55:10 +0100

    Add deck and hand structs

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include "SDL_render.h"
 #include <SDL.h>
 #include <SDL_image.h>
+#include <stdlib.h>
 
 // #define __PSP__
 
@@ -15,8 +17,10 @@
 
 #endif
 
-#define CARD_WIDTH 48
-#define CARD_HEIGHT 64
+#define CARD_SPRITE_WIDTH 48
+#define CARD_SPRITE_HEIGHT 64
+#define CARD_WIDTH 96
+#define CARD_HEIGHT 128
 
 SDL_Texture *cards_atlas;
 
@@ -37,12 +41,35 @@ typedef enum {
   RANK_KING
 } Rank;
 
+typedef struct {
+  Suit suit;
+  Rank rank;
+} Card;
+
+typedef struct {
+  short size;
+  short count;
+  Card *cards;
+} Hand, Deck;
+
 void draw_card(SDL_Renderer *renderer, Suit suit, Rank rank, SDL_Rect *dst) {
-  SDL_Rect src = {.x = rank * CARD_WIDTH,
-                  .y = suit * CARD_HEIGHT,
-                  .w = CARD_WIDTH,
-                  .h = CARD_HEIGHT};
+  SDL_Rect src = {.x = rank * CARD_SPRITE_WIDTH,
+                  .y = suit * CARD_SPRITE_HEIGHT,
+                  .w = CARD_SPRITE_WIDTH,
+                  .h = CARD_SPRITE_HEIGHT};
   SDL_RenderCopy(renderer, cards_atlas, &src, dst);
+}
+
+void draw_hand(SDL_Renderer *renderer, const Hand *hand) {
+  int hand_width = hand->count * (CARD_WIDTH - 16);
+  for (int i = 0; i < hand->count; i++) {
+    draw_card(renderer, hand->cards[i].suit, hand->cards[i].rank,
+              &(SDL_Rect){.x = SCREEN_WIDTH / 2 - hand_width / 2 +
+                               (CARD_WIDTH - 16) * i,
+                          .y = SCREEN_HEIGHT - CARD_HEIGHT - 16,
+                          .w = CARD_WIDTH,
+                          .h = CARD_HEIGHT});
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -59,11 +86,21 @@ int main(int argc, char *argv[]) {
   cards_atlas = SDL_CreateTextureFromSurface(renderer, cards_pixels);
   SDL_FreeSurface(cards_pixels);
 
-  SDL_Rect card_dst2 = {SCREEN_WIDTH / 2 - 48, SCREEN_HEIGHT / 2 - 64, 96, 128};
-  SDL_Rect card_dst1 = card_dst2;
-  card_dst1.x -= 100;
-  SDL_Rect card_dst3 = card_dst2;
-  card_dst3.x += 100;
+  srand(time(NULL));
+
+  Deck deck = {
+      .size = 52, .cards = (Card *)malloc(52 * sizeof(Card)), .count = 0};
+  for (int i = 0; i < 52; i++) {
+    deck.cards[deck.count] = (Card){.suit = i % 4, .rank = i % 13};
+    deck.count++;
+  }
+
+  Hand hand = {
+      .size = 5, .cards = (Card *)malloc(5 * sizeof(Card)), .count = 0};
+  for (int i = 0; i < hand.size; i++) {
+    hand.cards[hand.count] = deck.cards[rand() % 52];
+    hand.count++;
+  }
 
   int running = 1;
   SDL_Event event;
@@ -97,10 +134,7 @@ int main(int argc, char *argv[]) {
     // Clear the screen
     SDL_RenderClear(renderer);
 
-    // Draw a card
-    draw_card(renderer, SUIT_DIAMONDS, RANK_JACK, &card_dst1);
-    draw_card(renderer, SUIT_SPADES, RANK_ACE, &card_dst2);
-    draw_card(renderer, SUIT_CLUBS, RANK_KING, &card_dst3);
+    draw_hand(renderer, &hand);
 
     // Draw everything on a white background
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
