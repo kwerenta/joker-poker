983922723bd4464c6ceccdbcfd80bcb979c9d4bf
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-06-08 19:48:06 +0200

    Add poker hand stats overlay with layout lib

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 #define CLAY_IMPLEMENTATION
 #include <clay.h>
 
+#define LAY_IMPLEMENTATION
 #include "debug.h"
 #include "game.h"
 #include "gfx.h"
+#include "lib/layout.h"
 #include "renderer.h"
 #include "state.h"
 #include "system.h"
@@ -69,6 +71,10 @@ int main(int argc, char *argv[]) {
 #endif
 
   log_message(LOG_INFO, "Starting main loop...");
+
+  lay_context ctx;
+  lay_init_context(&ctx);
+  lay_reserve_items_capacity(&ctx, 512);
 
   while (state.running) {
     handle_controls();
@@ -143,7 +149,83 @@ int main(int argc, char *argv[]) {
     Clay_RenderCommandArray render_commands = Clay_EndLayout();
     execute_render_commands(render_commands);
 
-    render_overlay_poker_hands();
+    lay_id root = lay_item(&ctx);
+    lay_set_size_xy(&ctx, root, SCREEN_WIDTH, SCREEN_HEIGHT);
+    lay_set_contain(&ctx, root, LAY_ROW);
+
+    lay_id sidebar = lay_item(&ctx);
+    lay_insert(&ctx, root, sidebar);
+    lay_set_size_xy(&ctx, sidebar, SIDEBAR_WIDTH, 0);
+    lay_set_behave(&ctx, sidebar, LAY_VFILL);
+    lay_set_contain(&ctx, sidebar, LAY_COLUMN);
+
+    lay_id game_view = lay_item(&ctx);
+    lay_insert(&ctx, root, game_view);
+    lay_set_behave(&ctx, game_view, LAY_HFILL | LAY_VFILL);
+
+    lay_id overlay = lay_item(&ctx);
+    lay_set_size_xy(&ctx, overlay, SCREEN_WIDTH, SCREEN_HEIGHT);
+    lay_set_contain(&ctx, overlay, LAY_COLUMN | LAY_MIDDLE);
+
+    lay_id overlay_content = lay_item(&ctx);
+    lay_insert(&ctx, overlay, overlay_content);
+    lay_set_contain(&ctx, overlay_content, LAY_COLUMN);
+    lay_set_behave(&ctx, overlay_content, LAY_HFILL);
+    lay_set_margins_ltrb(&ctx, overlay_content, 8, 8, 8, 8);
+
+    lay_id items[12] = {0};
+    items[0] = lay_item(&ctx);
+    lay_insert(&ctx, overlay_content, items[0]);
+
+    for (uint8_t i = 0; i < 12; i++) {
+      lay_set_size_xy(&ctx, items[i], 0, CHAR_HEIGHT + 4);
+      lay_set_contain(&ctx, items[i], LAY_ROW | LAY_START);
+      lay_set_behave(&ctx, items[i], LAY_HFILL);
+
+      lay_id level = lay_item(&ctx);
+      lay_insert(&ctx, items[i], level);
+      lay_set_size_xy(&ctx, level, CHAR_WIDTH * 7, 0);
+      lay_set_behave(&ctx, level, LAY_VFILL);
+
+      lay_id name = lay_item(&ctx);
+      lay_append(&ctx, level, name);
+      lay_set_behave(&ctx, name, LAY_VFILL | LAY_HFILL);
+      lay_set_contain(&ctx, name, LAY_MIDDLE);
+
+      if (i == 11) {
+        lay_set_margins_ltrb(&ctx, items[i], 4, 4, 4, 4);
+        continue;
+      }
+
+      lay_set_margins_ltrb(&ctx, items[i], 4, 4, 4, 0);
+      items[i + 1] = lay_item(&ctx);
+      lay_append(&ctx, items[i], items[i + 1]);
+    }
+
+    lay_run_context(&ctx);
+    lay_run_item(&ctx, overlay);
+
+    lay_vec4 sidebar_rect = lay_get_rect(&ctx, sidebar);
+    lay_vec4 overlay_rect = lay_get_rect(&ctx, overlay);
+    lay_vec4 overlay_content_rect = lay_get_rect(&ctx, overlay_content);
+
+    draw_rectangle(&(Rect){sidebar_rect[0], sidebar_rect[1], sidebar_rect[2], sidebar_rect[3]}, RGB(30, 39, 46));
+
+    draw_rectangle(&(Rect){overlay_rect[0], overlay_rect[1], overlay_rect[2], overlay_rect[3]}, RGBA(0, 0, 0, 150));
+    draw_rectangle(
+        &(Rect){overlay_content_rect[0], overlay_content_rect[1], overlay_content_rect[2], overlay_content_rect[3]},
+        RGBA(30, 39, 46, 200));
+
+    for (uint8_t i = 0; i < 12; i++) {
+      lay_vec4 rect = lay_get_rect(&ctx, items[i]);
+      draw_rectangle(&(Rect){rect[0], rect[1], rect[2], rect[3]}, RGBA(255, 255, 255, 30));
+
+      rect = lay_get_rect(&ctx, lay_first_child(&ctx, items[i]));
+      draw_rectangle(&(Rect){rect[0], rect[1], rect[2], rect[3]}, RGB(255, 255, 255));
+      draw_text("lvl. 1", &(Vector2){rect[0] + 2, rect[1] + 2}, 0xFF000000);
+    }
+
+    lay_reset_context(&ctx);
 
 #ifdef DEBUG_BUILD
     frame_time = (sceKernelGetSystemTimeWide() - curr_time) / 1000.0f;
@@ -152,6 +234,7 @@ int main(int argc, char *argv[]) {
     end_frame();
   }
 
+  lay_destroy_context(&ctx);
   destroy();
   log_shutdown();
 
