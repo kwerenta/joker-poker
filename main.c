2c7d4d35ec6da87ee9ba522715303d6e0a9f04da
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-06-10 08:42:29 +0200

    Add layout lib auto rendering

--- main.c
+++ main.c
@@ -22,6 +22,54 @@ static char __attribute__((aligned(16))) list[262144];
 
 State state;
 
+typedef enum { UI_ELEMENT_TYPE_RECTANGLE, UI_ELEMENT_TYPE_TEXT } UIElementType;
+
+typedef struct {
+  uint32_t color;
+} UIElementRectangle;
+
+typedef struct {
+  uint32_t color;
+  uint32_t bg_color;
+  char *content;
+} UIELementText;
+
+typedef struct {
+  UIElementType type;
+
+  union {
+    UIElementRectangle rectangle;
+    UIELementText text;
+  };
+} UIElement;
+UIElement ui_elements[512] = {0};
+
+void render_ui_node(lay_context *ctx, lay_id node) {
+  UIElement *element = &ui_elements[node];
+
+  if (node == LAY_INVALID_ID) return;
+
+  lay_vec4 rect = lay_get_rect(ctx, node);
+
+  if (element->type == UI_ELEMENT_TYPE_TEXT) {
+    draw_text(element->text.content, &(Vector2){rect[0], rect[1]}, element->text.color);
+    return;
+  }
+
+  if (element->type == UI_ELEMENT_TYPE_RECTANGLE) {
+    draw_rectangle(&(Rect){rect[0], rect[1], rect[2], rect[3]}, element->rectangle.color);
+    return;
+  }
+}
+
+void traverse_ui_tree(lay_context *ctx, lay_id node) {
+  if (node == LAY_INVALID_ID) return;
+
+  render_ui_node(ctx, node);
+  traverse_ui_tree(ctx, lay_first_child(ctx, node));
+  traverse_ui_tree(ctx, lay_next_sibling(ctx, node));
+}
+
 void init() {
   log_init();
 
@@ -155,6 +203,7 @@ int main(int argc, char *argv[]) {
 
     lay_id sidebar = lay_item(&ctx);
     lay_insert(&ctx, root, sidebar);
+    ui_elements[sidebar] = (UIElement){.type = UI_ELEMENT_TYPE_RECTANGLE, .rectangle = {.color = RGB(30, 39, 46)}};
     lay_set_size_xy(&ctx, sidebar, SIDEBAR_WIDTH, 0);
     lay_set_behave(&ctx, sidebar, LAY_VFILL);
     lay_set_contain(&ctx, sidebar, LAY_COLUMN);
@@ -164,11 +213,14 @@ int main(int argc, char *argv[]) {
     lay_set_behave(&ctx, game_view, LAY_HFILL | LAY_VFILL);
 
     lay_id overlay = lay_item(&ctx);
+    ui_elements[overlay] = (UIElement){.type = UI_ELEMENT_TYPE_RECTANGLE, .rectangle = {.color = RGBA(0, 0, 0, 150)}};
     lay_set_size_xy(&ctx, overlay, SCREEN_WIDTH, SCREEN_HEIGHT);
     lay_set_contain(&ctx, overlay, LAY_COLUMN | LAY_MIDDLE);
 
     lay_id overlay_content = lay_item(&ctx);
     lay_insert(&ctx, overlay, overlay_content);
+    ui_elements[overlay_content] =
+        (UIElement){.type = UI_ELEMENT_TYPE_RECTANGLE, .rectangle = {.color = RGBA(30, 39, 46, 150)}};
     lay_set_contain(&ctx, overlay_content, LAY_COLUMN);
     lay_set_behave(&ctx, overlay_content, LAY_HFILL);
     lay_set_margins_ltrb(&ctx, overlay_content, 8, 8, 8, 8);
@@ -178,17 +230,30 @@ int main(int argc, char *argv[]) {
     lay_insert(&ctx, overlay_content, items[0]);
 
     for (uint8_t i = 0; i < 12; i++) {
+      ui_elements[items[i]] =
+          (UIElement){.type = UI_ELEMENT_TYPE_RECTANGLE, .rectangle = {.color = RGBA(255, 255, 255, 30)}};
       lay_set_size_xy(&ctx, items[i], 0, CHAR_HEIGHT + 4);
       lay_set_contain(&ctx, items[i], LAY_ROW | LAY_START);
       lay_set_behave(&ctx, items[i], LAY_HFILL);
 
       lay_id level = lay_item(&ctx);
+      ui_elements[level] = (UIElement){.type = UI_ELEMENT_TYPE_RECTANGLE, .rectangle = {.color = RGB(255, 255, 255)}};
       lay_insert(&ctx, items[i], level);
       lay_set_size_xy(&ctx, level, CHAR_WIDTH * 7, 0);
       lay_set_behave(&ctx, level, LAY_VFILL);
 
+      lay_id level_text = lay_item(&ctx);
+      ui_elements[level_text] =
+          (UIElement){.type = UI_ELEMENT_TYPE_TEXT, .text = {.content = "lvl.1", .color = RGB(0, 0, 0)}};
+      lay_insert(&ctx, level, level_text);
+      lay_set_margins_ltrb(&ctx, level_text, 2, 2, 2, 2);
+      lay_set_behave(&ctx, level_text, LAY_LEFT | LAY_TOP);
+
       lay_id name = lay_item(&ctx);
+      ui_elements[name] =
+          (UIElement){.type = UI_ELEMENT_TYPE_TEXT, .text = {.content = "Poker hand", .color = RGB(255, 255, 255)}};
       lay_append(&ctx, level, name);
+      lay_set_margins_ltrb(&ctx, name, 2, 2, 0, 2);
       lay_set_behave(&ctx, name, LAY_VFILL | LAY_HFILL);
       lay_set_contain(&ctx, name, LAY_MIDDLE);
 
@@ -205,25 +270,8 @@ int main(int argc, char *argv[]) {
     lay_run_context(&ctx);
     lay_run_item(&ctx, overlay);
 
-    lay_vec4 sidebar_rect = lay_get_rect(&ctx, sidebar);
-    lay_vec4 overlay_rect = lay_get_rect(&ctx, overlay);
-    lay_vec4 overlay_content_rect = lay_get_rect(&ctx, overlay_content);
-
-    draw_rectangle(&(Rect){sidebar_rect[0], sidebar_rect[1], sidebar_rect[2], sidebar_rect[3]}, RGB(30, 39, 46));
-
-    draw_rectangle(&(Rect){overlay_rect[0], overlay_rect[1], overlay_rect[2], overlay_rect[3]}, RGBA(0, 0, 0, 150));
-    draw_rectangle(
-        &(Rect){overlay_content_rect[0], overlay_content_rect[1], overlay_content_rect[2], overlay_content_rect[3]},
-        RGBA(30, 39, 46, 200));
-
-    for (uint8_t i = 0; i < 12; i++) {
-      lay_vec4 rect = lay_get_rect(&ctx, items[i]);
-      draw_rectangle(&(Rect){rect[0], rect[1], rect[2], rect[3]}, RGBA(255, 255, 255, 30));
-
-      rect = lay_get_rect(&ctx, lay_first_child(&ctx, items[i]));
-      draw_rectangle(&(Rect){rect[0], rect[1], rect[2], rect[3]}, RGB(255, 255, 255));
-      draw_text("lvl. 1", &(Vector2){rect[0] + 2, rect[1] + 2}, 0xFF000000);
-    }
+    traverse_ui_tree(&ctx, root);
+    traverse_ui_tree(&ctx, overlay);
 
     lay_reset_context(&ctx);
 
