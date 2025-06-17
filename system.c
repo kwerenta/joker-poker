71a4027df3be7c114c78d91e16b88f177b804bc7
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-06-17 22:19:00 +0200

    Add layout recalculating on any button click

--- system.c
+++ system.c
@@ -272,6 +272,7 @@ void handle_controls() {
 
   if (handle_navigation_controls() == 1 || state.overlay != OVERLAY_NONE) {
     state.controls.state = controls->data.Buttons;
+    state.render_commands = generate_render_commands();
     return;
   }
 
@@ -324,6 +325,7 @@ void handle_controls() {
   }
 
   controls->state = controls->data.Buttons;
+  state.render_commands = generate_render_commands();
 }
 
 void init_gu(char list[]) {
