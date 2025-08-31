8e06c44f97735fea9a52bc0ac8ae33cc5790ad9c
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-08-31 18:49:46 +0200

    Add basic suit related common jokers

--- content/joker.c
+++ content/joker.c
@@ -9,13 +9,53 @@ static void activate_joker_jolly(Joker *self) {
     state.game.selected_hand.score_pair.mult += 8;
 }
 
+static void activate_greedy_joker(Joker *self, Card *card) {
+  if (card->suit == SUIT_DIAMONDS) state.game.selected_hand.score_pair.mult += 3;
+}
+
+static void activate_lusty_joker(Joker *self, Card *card) {
+  if (card->suit == SUIT_HEARTS) state.game.selected_hand.score_pair.mult += 3;
+}
+
+static void activate_wrathful_joker(Joker *self, Card *card) {
+  if (card->suit == SUIT_SPADES) state.game.selected_hand.score_pair.mult += 3;
+}
+
+static void activate_gluttonous_joker(Joker *self, Card *card) {
+  if (card->suit == SUIT_CLUBS) state.game.selected_hand.score_pair.mult += 3;
+}
+
 const Joker JOKERS[] = {
     {.id = JOKER_JOKER,
      .name = "Joker",
      .description = "+4 mult when scored",
-     .base_price = 3,
+     .base_price = 2,
      .rarity = RARITY_COMMON,
      .activate_independent = activate_joker_joker},
+    {.id = JOKER_GREEDY,
+     .name = "Greedy Joker",
+     .description = "Played cards with Diamond suit give +3 Mult when scored",
+     .base_price = 5,
+     .rarity = RARITY_COMMON,
+     .activate_on_scored = activate_greedy_joker},
+    {.id = JOKER_LUSTY,
+     .name = "Lusty Joker",
+     .description = "Played cards with Heart suit give +3 Mult when scored",
+     .base_price = 5,
+     .rarity = RARITY_COMMON,
+     .activate_on_scored = activate_lusty_joker},
+    {.id = JOKER_WRATHFUL,
+     .name = "Wrathful Joker",
+     .description = "Played cards with Spade suit give +3 Mult when scored",
+     .base_price = 5,
+     .rarity = RARITY_COMMON,
+     .activate_on_scored = activate_wrathful_joker},
+    {.id = JOKER_GLUTTONOUS,
+     .name = "Gluttonous Joker",
+     .description = "Played cards with Club suit give +3 Mult when scored",
+     .base_price = 5,
+     .rarity = RARITY_COMMON,
+     .activate_on_scored = activate_gluttonous_joker},
     {.id = JOKER_JOLLY,
      .name = "Jolly Joker",
      .description = "+8 mult when scored hand contains pair",
